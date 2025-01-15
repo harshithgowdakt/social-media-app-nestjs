8c034be2526eeeac087282c8a08c370536fc2591
Harshith Gowda <harshithgowdakt@gmail.com>
2025-01-15 08:08:56 +0530

    fixed the module issue and added pg package

--- src/follower/follower.module.ts
+++ src/follower/follower.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { FollowerController } from './follower.controller';
+import { FollowerService } from './follower.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Follower } from './follower.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Follower])],
+  controllers: [FollowerController],
+  providers: [FollowerService],
+})
 export class FollowerModule {}
--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { LikeController } from './like.controller';
+import { LikeService } from './like.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Like } from './like.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Like])],
+  controllers: [LikeController],
+  providers: [LikeService],
+})
 export class LikeModule {}
